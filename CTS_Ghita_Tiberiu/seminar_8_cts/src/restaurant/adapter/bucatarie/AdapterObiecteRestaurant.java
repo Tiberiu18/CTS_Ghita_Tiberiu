a367cdbd89cf46749faec7ffbc5cbddc0b2c23d4
Tiberiu18 <ttiberiu48@yahoo.com>
2023-04-20 20:38:05 +0300

    seminar 8

---
+++ CTS_Ghita_Tiberiu/seminar_8_cts/src/restaurant/adapter/bucatarie/AdapterObiecteRestaurant.java
@@ -0,0 +1,17 @@
+package restaurant.adapter.bucatarie;
+
+import restaurant.adapter.bar.ISoftBar;
+import restaurant.adapter.bar.SoftBar;
+
+public class AdapterObiecteRestaurant implements ISoftRestaurant{
+    private ISoftBar softBar;
+
+    public AdapterObiecteRestaurant(ISoftBar softBar) {
+        this.softBar = softBar;
+    }
+
+    @Override
+    public void printeazaNota(double totalSuma) {
+        this.softBar.printeazaNotaBauturi(totalSuma);
+    }
+}
